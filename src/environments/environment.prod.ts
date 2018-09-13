c0a38eafc332e75bd5b43082df793e3a6ff6cfc3
Lkabeer <ahmed@Lkabeer.com>
2018-09-13 16:50:15 +0200

    version 0.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyCbdlei4BbA7QR33w7lirbNUHU_BAfNrTA",
+      authDomain: "xshop-fd261.firebaseapp.com",
+      databaseURL: "https://xshop-fd261.firebaseio.com",
+      projectId: "xshop-fd261",
+      storageBucket: "xshop-fd261.appspot.com",
+      messagingSenderId: "442470072811"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCbdlei4BbA7QR33w7lirbNUHU_BAfNrTA",
+    authDomain: "xshop-fd261.firebaseapp.com",
+    databaseURL: "https://xshop-fd261.firebaseio.com",
+    projectId: "xshop-fd261",
+    storageBucket: "xshop-fd261.appspot.com",
+    messagingSenderId: "442470072811"
+  }
 };
 
 /*
